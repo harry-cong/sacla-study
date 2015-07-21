0092cd0ab773be8be0cb1678bcd4281b9ec5a2f5
hyeongseok.heo <hyeongseok.heo@samsung.com>
2015-07-22 00:38:18 +0900

    http://docs.scala-lang.org/ko/tutorials/scala-for-java-programmers.html

--- StringExternalize/src/Hello.scala
+++ StringExternalize/src/Hello.scala
@@ -1,8 +1,15 @@
-
-
-/**
- * @author harry
- */
-object Hello extends App{
-  println("Hello, World!");
+object Time{
+  def oncePerSecond(callback: () => Unit ){
+    while (true) {
+      callback(); Thread sleep 1000
+    }
+  }
+  
+  def timeFlies(){
+    println("Time flies like an arrow...")
+  }
+  
+  def main(args: Array[String]): Unit = {
+   oncePerSecond { () => timeFlies() } 
+  }
 }
